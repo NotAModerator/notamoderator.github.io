c6ee1765377ad1fec457b7b16b33580852846fbe
NotAModerator <130181894+NotAModerator@users.noreply.github.com>
2025-11-10 21:39:57 -0500

    Add files via upload

---
+++ worm-tracker/index.js
@@ -0,0 +1,17 @@
+var counter = document.getElementById("counter");
+var uri = "https://api.sheetbest.com/sheets/7ecc7ab7-b42e-439a-92f8-3202a5ff2ee7"
+let total = 0
+
+function getCurrentCount() {
+	total = 0
+	fetch(uri)
+	.then((response) => response.json())
+	.then(function(data) {
+		data.forEach(function(v) {total += parseInt(v.Count)})
+		counter.innerText = total + "/?"
+	});
+}
+
+setInterval(getCurrentCount, 60000);
+
+getCurrentCount()
